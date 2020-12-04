b64ca100cd5dd38bb622363fcd093597825e18f0
123wjw <947631441.com>
2020-12-04 14:54:26 +0800

    Full pipeline test2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!!!Hello WJW'));
+app.get('/', (req, res) => res.send('Cal Cloud'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
